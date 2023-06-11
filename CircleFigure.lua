521749cdee0f52c1c50796c5be0a404d87c430fd
nazarpunk <nazarpunk@gmail.com>
2023-06-11 15:13:40 +0300

    lua update

--- CircleFigure.lua
+++ CircleFigure.lua
@@ -1,30 +1,66 @@
-MyClass = Class()
-
-function MyClass:init()
-    self.members = {}
+local function normalize (angle)
+    return (angle % (2 * math.pi) + 2 * math.pi) % (2 * math.pi);
 end
 
-function MyClass:__newindex(index, value)
-    if index == "testMember" then
-        self.members[index] = value
-        print("Set member " .. index .. " to " .. value)
-    else
-        rawset(self, index, value)
+---@class CircleFigure
+CircleFigure = {}
+
+---new
+---@param x number
+---@param y number
+---@param radius number
+---@param angle number
+---@param count number
+function CircleFigure:new(x, y, radius, angle, count)
+    local c = {
+        data = {},
+        edges = {}
+    };
+    setmetatable(c, {
+        __index = function(self, key)
+            if key == 'x' or key == 'y' or key == 'radius' or key == 'angle' then
+                return self.data[key];
+            end
+            return rawget(CircleFigure, key);
+        end,
+        __newindex = function(self, key, value)
+            if key == 'x' or key == 'y' then
+                self.data[key] = value;
+                return self:move();
+            end
+            if key == 'radius' then
+                self.data[key] = math.max(0, value)
+                return self:move();
+            end
+            if key == 'angle' then
+                self.data[key] = normalize(value)
+                return self:move();
+            end
+            if key == 'count' then
+                self.data[key] = math.max(3, value)
+                return self:move();
+            end
+            rawset(self, key, value)
+        end
+    });
+    c.x = x;
+    c.y = y;
+    c.radius = radius;
+    c.angle = angle;
+    c.count = count;
+    return c;
+end
+
+function CircleFigure:move()
+    local d = self.data;
+    if d.x == nil or d.y == nil or d.radius == nil or d.angle == nil or d.count == nil then
+        return
     end
+    print('move', d.x, d.y)
 end
 
-function MyClass:__index(index)
-    if index == "testMember" then
-        print("Getting " .. index)
-        return self.members[index]
-    else
-        return rawget(self, index)
-    end
-end
+local c = CircleFigure:new(10, 20, 50, 11, 1);
+c.x = 10;
+c.x = c.x + 10;
 
-foo = MyClass()
-
-foo.testMember = 5
-foo.testMember = 2
-
-print(foo.testMember)
+print(c.x, c.radius);
